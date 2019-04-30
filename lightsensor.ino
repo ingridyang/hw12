5d149035eb1da5ba96804f93913eadccf4bbe358
ingridyang <43019677+ingridyang@users.noreply.github.com>
2019-04-29 22:57:06 -0700

    Update lightsensor.ino

--- lightsensor.ino
+++ lightsensor.ino
@@ -24,3 +24,42 @@
     }
 
 
+//pt 2:
+
+int soundsensor = A0;
+int led1 = 7;
+int led2 = 10;
+
+void setup() {
+
+  pinMode (soundsensor, INPUT);
+  pinMode (led1, OUTPUT);
+  pinMode (led2, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  float val = analogRead (soundsensor);
+  Serial.print("0 1024 ");
+  Serial.println(val);
+
+  if (val > 300)
+  {
+    digitalWrite (led1, HIGH);
+  }
+  else
+  {
+    digitalWrite (led1, LOW);
+  }
+  
+  
+  if (val > 200)
+  {
+    digitalWrite (led2, HIGH);
+  }
+  else
+  {
+    digitalWrite (led2, LOW);
+  }
+}
